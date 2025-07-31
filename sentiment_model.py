42abbfd09ac1f4ca78daa415c2dd3b6359ff7972
Yogesh Verma <81632112+2533-Yogesh@users.noreply.github.com>
2025-07-31 14:46:48 +0530

    Update sentiment_model.py

--- sentiment_model.py
+++ sentiment_model.py
@@ -1,7 +1,11 @@
 from transformers import pipeline
 
-# Load sentiment-analysis pipeline
-classifier = pipeline("sentiment-analysis")
+# Use a specific model and revision
+classifier = pipeline(
+    "sentiment-analysis",
+    model="distilbert/distilbert-base-uncased-finetuned-sst-2-english",
+    revision="714eb0f"
+)
 
 def analyze_sentiment(text):
     result = classifier(text)[0]
